9720e08208b39cfdd25d56e9e68d536241348658
Bradley Martin <bradley.r.martin@me.com>
2022-02-24 19:37:36 +1100

    update

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,5 @@
 
 Route::get('{slug?}',function(){
     return view('bootstrap::app');
-})->where('slug', '(?!api|reactive).*(?<!js|css|json|html|txt|xml|webmanifest|jpg|gif|png|svg)')->middleware('web');
+})->where('slug', '(?!api|reactive|files).*(?<!js|css|json|html|txt|xml|webmanifest|jpg|gif|png|svg)')->middleware('web');
 
